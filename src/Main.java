c3e64addfc6a82de71fbe6b24a8f96254e78bdd5
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-10 15:08:29 +0100

    Finish Es 1

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,22 @@ import org.lessons.java.shop.Prodotto;
 public class Main {
     public static void main(String[] args) {
 
-       Prodotto macchina = new Prodotto("audi", "veloce come il vento",1543.56);
-       Prodotto macchina2 = new Prodotto("mercedes", "leggermente più veloce del vento",2356.76);
-        System.out.println(macchina);
-        System.out.println(macchina2);
+       Prodotto prodotto = new Prodotto("audi", "veloce come il vento",1573.56, 23);
+        System.out.println("Codice prodotto: " + prodotto.getCodice());
+        System.out.println("Nome prodotto: " + prodotto.getNome());
+        System.out.println("Descrizione prodotto: " + prodotto.getDescrizione());
+        System.out.println("Prezzo prodotto: " + prodotto.getPrezzo());
+        System.out.println("Prezzo ivato prodotto: " + prodotto.getPrezzoIvato());
+
+
+        prodotto.setNome("Audi v7-stonedition");
+        prodotto.setDescrizione("leggermente piu veloce del vento");
+        prodotto.setPrezzo(2220.55);
+        prodotto.setIva(25);
+
+        System.out.println("Nome prodotto aggiornato: " + prodotto.getNome());
+        System.out.println("Descrizione prodotto aggiornata: " + prodotto.getDescrizione());
+        System.out.println("Prezzo prodotto aggiornato: " + prodotto.getPrezzo());
+        System.out.println("Prezzo ivato prodotto aggiornato: " + prodotto.getPrezzoIvato());
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,6 @@
 package org.lessons.java.shop;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Prodotto {
@@ -9,34 +10,34 @@ public class Prodotto {
   //- descrizione
   //- prezzo
   //- iva
-    private int codice;
+    private final int codice;
     private String nome;
     private String descrizione;
     private double prezzo;
-    private final static int iva = 23;
+    private int iva;
 
     // COSTRUTTORI
-    public Prodotto(String nome, String descrizione, double prezzo) {
+    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
         this.codice = generateRandomCode();
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
+        this.iva = iva;
     }
 
-    // METODI
-    public int generateRandomCode() {
-        Random rand = new Random();
-        return rand.nextInt(100000000); // generiamo un numero a 8 cifre
-    }
+
 
     // GETTER e SETTER
-
+    //CODICE
+    public int getCodice() {
+        return this.codice;
+    }
     //NOME
     public String getNome() {
         return nome;
     }
 
-    public void setNome() {
+    public void setNome(String nome) {
         this.nome = nome;
     }
     //DESCRIZIONE
@@ -44,7 +45,7 @@ public class Prodotto {
         return descrizione;
     }
 
-    public void setDescrizione() {
+    public void setDescrizione(String descrizione) {
         this.descrizione = descrizione;
     }
 
@@ -53,8 +54,27 @@ public class Prodotto {
         return prezzo;
     }
 
-    public void setPrezzo() {
+    public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
+    }
+    public double getIva() {
+        return this.iva;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    // METODI
+    public int generateRandomCode() {
+        Random rand = new Random();
+        return rand.nextInt(100000000); // generiamo un numero a 8 cifre
+    }
+
+    public String getPrezzoIvato() {
+        double prezzoIvato = this.prezzo + (this.prezzo * this.iva / 100);
+        DecimalFormat df = new DecimalFormat("#0.00");
+        return df.format(prezzoIvato);
     }
 
     public String toString() {
